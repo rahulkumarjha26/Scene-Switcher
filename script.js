9838a8482781b2f1142942258147f7441dc83fa9
Rahul Jha <rahulkumarjha26@gmail.com>
2024-09-02 01:31:16 +0530

    changes

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 document.getElementById("menu-button").addEventListener("click", function () {
+  document.getElementById("modal").style.display = "block";
   document.getElementById("modal").style.width = "100%";
 });
 
 document.getElementById("close-button").addEventListener("click", function () {
   document.getElementById("modal").style.width = "0";
+  document.getElementById("modal").style.display = "none";
 });
