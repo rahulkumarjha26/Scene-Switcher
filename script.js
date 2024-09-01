0de69b490c6dbf72a72a007470e4e3887bff7591
Rahul Jha <rahulkumarjha26@gmail.com>
2024-09-02 01:23:12 +0530

    modal changes

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("menu-button").addEventListener("click", function () {
+  document.getElementById("modal").style.width = "100%";
+});
+
+document.getElementById("close-button").addEventListener("click", function () {
+  document.getElementById("modal").style.width = "0";
+});
