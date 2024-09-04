c1dcf372286e428797ec05ad975b4c273d250e8e
Rahul Jha <rahulkumarjha26@gmail.com>
2024-09-04 18:32:44 +0530

    font color and bg changes added

--- backend.js
+++ backend.js
@@ -2,13 +2,16 @@ const colorPicker = document.getElementById("favcolor-1");
 const colorPickerSub = document.getElementById("favcolor-2");
 const colorDisplay = document.getElementById("color-value-1");
 const colorDisplaySub = document.getElementById("color-value-2");
+const subtitleText = document.getElementById("subtitle-text");
 
 colorPicker.addEventListener("input", function () {
   colorDisplay.value = colorPicker.value;
+  subtitleText.style.color = `${colorPicker.value}`;
 });
 
 colorPickerSub.addEventListener("input", function () {
   colorDisplaySub.value = colorPickerSub.value;
+  subtitleText.style.backgroundColor = `${colorPickerSub.value}`;
 });
 
 colorDisplay.addEventListener("input", function () {
@@ -27,5 +30,5 @@ const sliderValue = document.getElementById("slider-val");
 rangeSlider.addEventListener("input", function () {
   const value = rangeSlider.value;
   sliderValue.textContent = value;
-  document.getElementById("subtitle-text").style.fontSize = `${value / 2}px`;
+  subtitleText.style.fontSize = `${value / 2}px`;
 });
